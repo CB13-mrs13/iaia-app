75b7912cea48142f0785725f4abe0f91029cce27
bruno cousin <cousinb13@gmail.com>
2025-06-26 22:47:06 +0000

    il y a sans doute un problème de css dans le code ? l'affichage en versi

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <Link
           href="/"
-          className="mr-6 flex items-center space-x-2"
+          className="mr-2 flex items-center space-x-2 sm:mr-6"
           aria-label="Homepage"
         >
           <Image
@@ -27,7 +27,7 @@ export default function Navbar() {
             className="h-10 w-10"
             priority // Ajout de la propriété priority pour le LCP
           />
-          <span className="font-bold text-2xl sm:inline-block">IAIA</span>
+          <span className="hidden font-bold text-2xl sm:inline-block">IAIA</span>
         </Link>
 
         <nav className="flex flex-1 items-center space-x-4">
@@ -37,7 +37,7 @@ export default function Navbar() {
           </Link>
           */}
         </nav>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-1 sm:space-x-2">
           <LanguageSwitcher />
           {loading ? (
             <div className="h-9 w-24 animate-pulse rounded-md bg-muted"></div>
@@ -46,8 +46,9 @@ export default function Navbar() {
           ) : (
             <>
               <Button variant="ghost" asChild>
-                <Link href="/login">
-                  <LogIn className="mr-2 h-4 w-4" /> Login
+                <Link href="/login" aria-label="Login">
+                  <LogIn className="h-5 w-5 sm:mr-2" />
+                  <span className="hidden sm:inline">Login</span>
                 </Link>
               </Button>
               <Button asChild>
