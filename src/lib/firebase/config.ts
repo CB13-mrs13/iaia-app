aa3b284942d976528d33039e955cef13c32db248
bruno cousin <cousinb13@gmail.com>
2025-06-27 15:17:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,7 +2,6 @@
 import { initializeApp, getApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
-import { getStorage, type FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -27,6 +26,5 @@ if (!getApps().length) {
 
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
-const storage: FirebaseStorage = getStorage(app);
 
-export { app, auth, db, storage };
+export { app, auth, db };
