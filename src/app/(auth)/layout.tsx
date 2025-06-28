eb702e78dc3f6cf9a91f5c4bbbd67e3731d76b0d
bruno cousin <cousinb13@gmail.com>
2025-06-28 11:16:42 +0000

    je suis dépité et à 2 doigts d'abandonner :(

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-[calc(100vh-theme(spacing.32))] items-center justify-center py-12 animate-fadeIn">
+    <div className="flex min-h-screen items-center justify-center p-4">
       {children}
     </div>
   );
