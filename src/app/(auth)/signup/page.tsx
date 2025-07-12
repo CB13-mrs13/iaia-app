2bb0cb8a1c86500bac464e8dc602527fe76dc463
bruno cousin <cousinb13@gmail.com>
2025-07-12 10:27:26 +0000

    pardon je recommence mon explication j’ai fait moi aussi une mauvaise ma

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -44,7 +44,7 @@ export default function SignUpPage() {
     setShowConfetti(true);
   };
   
-  if (loading || user) {
+  if (loading || (user && !showConfetti)) {
     return (
        <div className="flex min-h-screen items-center justify-center">
         <Loader2 className="h-8 w-8 animate-spin text-primary" />
