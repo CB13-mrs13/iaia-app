7cf0929da4935bf260b1ef90a32dd8b7ad322998
CB13-mrs13 <cb13fr@gmail.com>
2026-02-09 22:17:06 +0100

    Security: stop tracking env files

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {},
-  // SUPPRIME la section "experimental" ou du moins "devTools"
-  // experimental: {
-  //   devTools: false,
-  // 06-10-2025 essai pour re-synchroniser avec firebase },
 };
 
 export default nextConfig;
